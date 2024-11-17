9bec8ed671040a93def66720b1c15adb503d18a1
carisaelam <carisaelam@gmail.com>
2024-11-17 14:10:23 -0500

    Add jumpToSlide function

--- script.js
+++ script.js
@@ -4,12 +4,15 @@ const carouselContainer = document.querySelector('.carousel__container');
 const slideImages = document.querySelectorAll('.slide__image');
 const backButton = document.querySelector('.back__button');
 const nextButton = document.querySelector('.next__button');
+const slides = carouselContainer.querySelectorAll('.slide');
+const positionButtons = document.querySelectorAll('.position__button');
 
 let slidePosition = 0;
 
-// Event listeners
-nextButton.addEventListener('click', (e) => {
-  const slides = carouselContainer.querySelectorAll('.slide');
+// Functions
+
+// Go forward one slide
+function next() {
   if (slidePosition <= slides.length - 2) {
     slidePosition++;
 
@@ -17,14 +20,39 @@ nextButton.addEventListener('click', (e) => {
       slide.style.transform = `translateX(calc(-${slidePosition * 100}% - 1rem))`;
     });
   }
-});
+}
 
-backButton.addEventListener('click', (e) => {
-  const slides = carouselContainer.querySelectorAll('.slide');
-
+// Go back one slide
+function back() {
   slidePosition--;
 
   slides.forEach((slide) => {
     slide.style.transform = `translateX(calc(-${slidePosition * 100}% - 1rem))`;
+  });
+}
+
+function jumpToSlide(position) {
+  slidePosition = position;
+
+  slides.forEach((slide) => {
+    slide.style.transform = `translateX(calc(-${slidePosition * 100}% - 1rem))`;
+  });
+}
+
+// Event listeners
+nextButton.addEventListener('click', () => {
+  next();
+});
+
+backButton.addEventListener('click', () => {
+  back();
+});
+
+positionButtons.forEach((positionButton) => {
+  positionButton.addEventListener('click', (e) => {
+    position = e.target.textContent - 1;
+    console.log('position', position);
+
+    jumpToSlide(position);
   });
 });
